cf3561a15b565f4af58c7dcf83a415fd7a28e03b
MatiCarrera <mcarreradev12@gmail.com>
2025-11-03 18:08:15 -0300

    new pink colors

--- app/[locale]/(marketing)/_components/hours-saved-section.tsx
+++ app/[locale]/(marketing)/_components/hours-saved-section.tsx
@@ -68,7 +68,7 @@ export default function HoursSavedSection() {
       >
         <h2 className="text-4xl font-bold leading-tight">
           <span className="text-muted-foreground">{t("title.part1")} </span>
-          <span className="bg-gradient-to-r from-primary to-purple-500 text-transparent bg-clip-text">
+          <span className="bg-gradient-to-r from-pink-500 to-purple-600 text-transparent bg-clip-text">
             <CountUp end={164} duration={2.5} />h
           </span>{" "}
           {t("title.part2")}
