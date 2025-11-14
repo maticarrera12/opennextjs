ac8c19fb9d24532d5d20993084b56e9cad3d074f
MatiCarrera <mcarreradev12@gmail.com>
2025-11-14 18:33:35 -0300

    changed waitlist button for cta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,5 +26,7 @@ export const metadata: Metadata = {
   },
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  <html>
+    <body>{children}</body>
+  </html>;
 }
